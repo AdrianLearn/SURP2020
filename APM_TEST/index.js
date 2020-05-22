13227d817f30e81f562a1768c090f48d7e625900
Adrian Leung <adrian.leungssw@gmail.com>
2020-05-22 13:26:23 -0700

    slightly cleaned and commented code

--- APM_TEST/index.js
+++ APM_TEST/index.js
@@ -1,10 +1,13 @@
 const canvas = document.querySelector("canvas");
 const ctx = canvas.getContext("2d");
 let time = document.getElementById("time");
-let currTarget = 50;
-let finished = false;
-let first = true;
-let timef = null;
+document.body.addEventListener("mousemove", updateMouse);
+let mouse = { x: 0, y: 0 }; // mouse object containing coordinates of the mouse location on the canvas
+let currTarget = 50;  // tracks what target you are currently clicking
+let finished = false; // tracks if you have finished the target array
+let first = true; // tracks if this is the first target you have clicked
+let timef = null; //tracks the number of milliseconds leading up to the start
+
 //Function called by p5.js in the beginning to set the canvas
 function setup() {
     frameRate(100);
@@ -15,6 +18,7 @@ function setup() {
     time.innerHTML = 0;
     stack = new Stack;
     stack.populate();
+    //something wrong with this
     targets = [
       new Target(random(canvas.width-30),random(canvas.height-30),stack.pop()),
       new Target(random(canvas.width-30),random(canvas.height-30),stack.pop()),
@@ -23,23 +27,57 @@ function setup() {
       new Target(random(canvas.width-30),random(canvas.height-30),stack.pop()),
     ];
     moveTargets();
-    targets.forEach(target => target.draw());
-
 }
 
 function random(range){
   return Math.floor(Math.random() * (range-30)) + 30;
 }
 
-function updateMouse(event) {
-  const { left, top } = canvas.getBoundingClientRect();
-  mouse.x = event.clientX - left;
-  mouse.y = event.clientY - top;
+/*
+* function to check and move targets if they overlap
+*/
+function moveTargets(){
+  for (let i = 0; i < targets.length; i++) {
+    for (let j = i+1; j < targets.length; j++) {
+      pushOff(targets[i], targets[j]);
+    }
+  }
 }
 
+/*
+* Function to push two targets off each other if they happen to spawn on one another
+*/
+function pushOff(c1, c2) {
+  let [dx, dy] = [c2.x - c1.x, c2.y - c1.y];
+  const L = Math.hypot(dx, dy);
+  let distToMove = 50 - L;
+  if (distToMove > 0) {
+    dx /= L;
+    dy /= L;
+    c1.x -= dx * distToMove / 2;
+    c1.y -= dy * distToMove / 2;
+    c2.x += dx * distToMove / 2;
+    c2.y += dy * distToMove / 2;
+  }
+  clearBackground();
+  targets.forEach(target => target.draw());
+}
+
+/*
+* Function to clear the background to avoid clutter of previous targets
+*/
+function clearBackground() {
+  ctx.fillStyle = 'grey';
+  ctx.fillRect(0,0,canvas.width, canvas.height);
+}
+
+/*
+* Function to check if the mouse is within the area of the target
+*/
 function mouseBounds(){
   return mouse.x <= targets[0].x + 20 && mouse.x >= targets[0].x - 20 && mouse.y <= targets[0].y +20 && mouse.y >= targets[0].y-20;
 }
+
 canvas.addEventListener("click", () => {
   if(targets[0].index == currTarget && mouseBounds()){
     if(currTarget <= 5){
@@ -59,50 +97,24 @@ canvas.addEventListener("click", () => {
   }
 });
 
-function pushOff(c1, c2) {
-  let [dx, dy] = [c2.x - c1.x, c2.y - c1.y];
-  const L = Math.hypot(dx, dy);
-  let distToMove = 50 - L;
-  if (distToMove > 0) {
-    dx /= L;
-    dy /= L;
-    c1.x -= dx * distToMove / 2;
-    c1.y -= dy * distToMove / 2;
-    c2.x += dx * distToMove / 2;
-    c2.y += dy * distToMove / 2;
-  }
-  clearBackground();
-  targets.forEach(target => target.draw());
-
-}
-
-function clearBackground() {
-  ctx.fillStyle = 'grey';
-  ctx.fillRect(0,0,canvas.width, canvas.height);
-}
-
-let mouse = { x: 0, y: 0 }; // mouse object containing coordinates of the mouse location on the canvas
-document.body.addEventListener("mousemove", updateMouse);
-
+/*
+* function to increment the timer, chopping off everything past the hundredth of a second, and update the span
+*/
 function increaseTime(){
   if(!finished){
-  const t = millis()/1000-timef;
-  const x = t.toFixed(2);
-  time.innerHTML = x;
-  time = document.getElementById("time");
-}
-}
-
-function moveTargets(){
-  for (let i = 0; i < targets.length; i++) {
-  for (let j = i+1; j < targets.length; j++) {
-    pushOff(targets[i], targets[j]);
+    const t = millis()/1000-timef;
+    const x = t.toFixed(2);
+    time.innerHTML = x;
+    time = document.getElementById("time");
   }
 }
-}
+
+/*
+* function called by the framerate, handles time, moving of targets if necessary, and when the program is finished
+*/
 function draw(){
   if(first){
-  timef = millis()/1000;
+    timef = millis()/1000;
   }
   increaseTime();
   moveTargets();
@@ -113,6 +125,16 @@ function draw(){
     ctx.fillText(`Finished`, (canvas.width/3), canvas.height / 2);
   }
 }
+/*
+* Function to track mouse movement
+*/
+function updateMouse(event) {
+  const { left, top } = canvas.getBoundingClientRect();
+  mouse.x = event.clientX - left;
+  mouse.y = event.clientY - top;
+}
+
+
 // Subclasses for stack and target
 class Stack{
   constructor(){
