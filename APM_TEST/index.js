634fbcd24f0019d21d0ae6dd23735b34f3e257c0
Adrian Leung <adrian.leungssw@gmail.com>
2020-05-21 17:38:36 -0700

    targets now disappear and appear when you click the largest number

--- APM_TEST/index.js
+++ APM_TEST/index.js
@@ -4,12 +4,13 @@ let time = document.getElementById("time");
 const ellipseSize = 2;
 let timer = time.innerHTML;
 let frames = 0;
-
+let currIndex = 0;
+let currTarget = 50;
 /*
 * required by p5.js, called at the beginning of the webpage load
 */
 function setup() {
-    frameRate(60);
+    frameRate(1);
     noStroke();
     ctx.fillStyle = 'grey';
     ctx.fillRect(0,0,canvas.width, canvas.height);
@@ -18,15 +19,18 @@ function setup() {
     stack = new Stack;
     stack.populate();
     targets = [
-      new Target(random(760),random(560),stack.pop()),
-      new Target(random(760),random(560),stack.pop()),
-      new Target(random(760),random(560),stack.pop()),
+      new Target(random(780),random(580),stack.pop()),
+      new Target(random(780),random(580),stack.pop()),
+      new Target(random(780),random(580),stack.pop()),
+      new Target(random(780),random(580),stack.pop()),
+      new Target(random(780),random(580),stack.pop()),
     ];
+    targets.forEach(target => target.show());
     //requestAnimationFrame(drawScene);
 }
 
 function random(range){
-  return Math.floor(Math.random()*range);
+  return Math.floor(Math.random() * range-20) + 20;
 }
 
 function updateMouse(event) {
@@ -62,39 +66,60 @@ class Target{
     Object.assign(this, {x,y,index});
   }
   show(){
+    noStroke();
     ctx.fillStyle = 'red';
     ctx.beginPath();
-    ctx.arc(this.x,this.y, 20, 0, Math.PI * 2);
+    ctx.arc(this.x,this.y, 24, 0, Math.PI * 2);
     ctx.fill();
     ctx.fillStyle = 'black';
     ctx.font = "10px Arial";
     ctx.strokeText(this.index,this.x-5,this.y+4);
     ctx.stroke();
   }
+  hide(){
+    noStroke();
+    ctx.fillStyle = 'grey';
+    ctx.beginPath();
+    ctx.arc(this.x,this.y, 25, 0, Math.PI * 2);
+    ctx.fill();
+  }
 }
-let targets = [
+targets = [
   new Target(random(780),random(580)),
   new Target(random(780),random(580)),
   new Target(random(780),random(580)),
   new Target(random(780),random(580)),
   new Target(random(780),random(580)),
 ];
+function mouseBounds(){
+  return mouse.x <= targets[currIndex].x + 20 && mouse.x >= targets[currIndex].x - 20 && mouse.y <= targets[currIndex].y +20 && mouse.y >= targets[currIndex].y-20;
+}
+canvas.addEventListener("click", () => {
+  console.log('click');
+  if(targets[currIndex].index == currTarget && mouseBounds()){
+    console.log(targets[currIndex].index);
+    console.log(currTarget);
+    let temp = new Target(random(780), random(580),stack.pop());
+    targets.push(temp);
+    temp.show();
+    targets[currIndex].hide();
+    currTarget--;
+    currIndex++;
+  }
+});
 
 let mouse = { x: 0, y: 0 }; // mouse object containing coordinates of the mouse location on the canvas
 document.body.addEventListener("mousemove", updateMouse);
 
 function increaseTime(){
   frames++;
-  if(frames%60===0){
-  timer++;
+  //if(frames%60===0){
+    timer++;
+  //}
   time.innerHTML = timer;
   time = document.getElementById("time");
 }
-}
+
 function draw(){
-  //console.log(getFrameRate());
-  //clearBackground();
   increaseTime();
-  targets.forEach(target => target.show());
-  //requestAnimationFrame(draw);
 }
